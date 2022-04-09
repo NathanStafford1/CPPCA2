f9ad02a765f8dc81d35bbc6ffb8c7d1a50201df2
Nathan <d00236171@student.dkit.ie>
2022-04-09 19:08:19 +0100

    Implemented feature 1

--- Image.cpp
+++ Image.cpp
@@ -11,7 +11,29 @@
 
 bool Image::load(string filename)
 {
+    ifstream ifs(filename,std::ios::binary);
+    string magicNumber;
+    ifs >> magicNumber;
+    ifs >> this->w;
+    ifs >> this->h;
+    int p;
+    ifs >> p;
+    ifs.ignore(256,'\n');
+    if (p==255)
+    {
+        delete[] pixels;
+        pixels = new Rgb[w*h];
+        unsigned char pix[3];
+        for (int i = 0; i < w*h; ++i)
+        {
+            ifs.read(reinterpret_cast<char *>(pix), 3);
+            this->pixels[i].r = pix[0];
+            this->pixels[i].g = pix[1];
+            this->pixels[i].b = pix[2];
 
+        }
+        return true;
+    }
     return false;
 
 }
